476a5044775f340dc7f89b7136217d830e99dcb5
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2019-01-02 20:31:48 -0500

    🤔 Complete Setup Express

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ import { restRouter } from './api'
 import { connect } from './db'
 import { signin, protect } from './api/modules/auth'
 // Declare an app from express
-const app = null
+const app = express()
+
+currentApp.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+currentApp.listen(3000, () => console.log('listening to express'))
 
 export default app
