55abbd5f255a8cc2020f0ca9a4b5ad615d5f7ecf
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2019-01-06 23:52:02 -0500

    🤔 Solve MongoDB models (lesson-5)

--- src/api/resources/playlist/playlist.model.js
+++ src/api/resources/playlist/playlist.model.js
@@ -1,7 +1,19 @@
 import mongoose from 'mongoose'
 
 export const schema = {
-  
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+  },
+  songs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'song'
+  }],
+  favorite: {
+    type: Boolean,
+    required: true,
+    default: false,
+  }
 }
 
 const playlistSchema = new mongoose.Schema(schema)
--- src/api/resources/song/song.model.js
+++ src/api/resources/song/song.model.js
@@ -1,7 +1,27 @@
 import mongoose from 'mongoose'
 
 export const schema = {
-  
+  title: {
+    type: String,
+    required: [true, 'oof']
+  },
+  url: {
+    type: String,
+    unique: true,
+    required: [true, 'Song must have an accompanying url']
+  },
+  album: {
+    type: String,
+  },
+  artist: {
+    type: String,
+  },
+  rating: {
+    type: Number
+  },
+  favorite: {
+    type: Boolean
+  }
 }
 
 const songSchema = new mongoose.Schema(schema)
